d451556d0902ea69d8f3071dad05d42721762467
ramaputra1 <rama.aptra@gmail.com>
2026-06-19 10:50:52 -1000

    phase2 - implemented CoinGeckoClient class, it has single async method getTrendingData() send http get req to CoinGecko endpoint. Just how to get data from api then return in decoded JSON.

--- dartpedia/crypto/lib/api/coingecko_client.dart
+++ dartpedia/crypto/lib/api/coingecko_client.dart
@@ -1,3 +1,19 @@
-class CoingeckoClient {
-  // TODO: implement CoinGecko API calls
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class CoinGeckoClient {
+  static const _baseUrl = 'https://api.coingecko.com/api/v3';
+
+  Future<Map<String, dynamic>> getTrendingData() async {
+    final url = Uri.parse('$_baseUrl/search/trending');
+    final response = await http.get(url);
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        'Failed to fetch trending data: ${response.statusCode}',
+      );
+    }
+
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  }
 }

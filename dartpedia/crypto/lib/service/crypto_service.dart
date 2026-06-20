33e515f74ec8292a728d5fd3b5f327b58defb079
ramaputra1 <rama.aptra@gmail.com>
2026-06-20 00:30:12 -1000

    phase2 - added services to hand the http call to CoinGeckiClient.getCoinDetail() and will return parsed CoinDetail object

--- dartpedia/crypto/lib/service/crypto_service.dart
+++ dartpedia/crypto/lib/service/crypto_service.dart
@@ -1,4 +1,5 @@
 import '../api/coingecko_client.dart';
+import '../model/coin_detail.dart';
 import '../model/trending_coin.dart';
 
 class CryptoService {
@@ -12,5 +13,10 @@ class CryptoService {
     return coins
         .map((c) => TrendingCoin.fromJson(c as Map<String, dynamic>))
         .toList();
+  }
+
+  Future<CoinDetail> getCoinDetail(String id) async {
+    final data = await _client.getCoinDetail(id);
+    return CoinDetail.fromJson(data);
   }
 }

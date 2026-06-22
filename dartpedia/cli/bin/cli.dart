bf3947052cb6a2f61547fc36a5fc27f7be0ed30b
MOKOJOPE <momoinbb21@gmail.com>
2026-06-22 09:04:28 -1000

    Finalize the searchWikipedia function

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -33,7 +33,9 @@ void searchWikipedia(List<String>? arguments) {
     articleTitle = arguments.join(' ');
   }
 
-  print('Current article title: $articleTitle');
+  print('Looking up articles about "articleTitle". Please wait.');
+  print('Here ya go!');
+  print('Pretend this is an article about "$articleTitle")');
 }
 
 void printUsage(){  //printUsage Function: To make the output more user-friendly, create a separate function to display usage information. 
